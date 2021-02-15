feeb652c1cf788daa010f38524e7aad3ec695054
masa <masa@masas-macbook-pro.local>
2021-02-15 23:03:51 +0900

    Add brew_install.sh

---
+++ brew_install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+# CL essentials
+brew install tldr
+brew install tree
+brew install bat
+brew install exa
+brew install htop
+brew install git
+
+# others
+brew tap homebrew/cask-fonts
+brew install --cask font-hack-nerd-font
