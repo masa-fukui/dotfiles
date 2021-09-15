fb35f8a38683bbc991fcb73ef38bb0c2e15b21c1
Masa Fukui <fkmkfkmasum41@gmail.com>
2021-09-15 14:34:48 +0900

    add zsh-syntax-highlighting instruction

--- setup.sh
+++ setup.sh
@@ -8,3 +8,7 @@ git clone https://github.com/dracula/vim.git dracula
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim \
     --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+# install zsh-syntax-highlighting with oh-my-zsh
+# after running git clone add the replace the pulugins=... with plugins=(git zsh-syntax-highlighting)
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
