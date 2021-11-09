76d9b1f58fa80a0e9738b1a9fd945c5386080022
Masa (Mark) Fukui <fkmkfkmasum41@gmail.com>
2021-11-09 13:46:27 +0900

    adds zsh-z plugin command to setup.sh

--- setup.sh
+++ setup.sh
@@ -12,3 +12,8 @@ curl -fLo ~/.local/share/nvim/site/autoload/plug.vim \
 # after running git clone add the replace the pulugins=... with plugins=(git zsh-syntax-highlighting)
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
+
+# install zsh-z plugin
+# reuires you to add zsh-z to zsh plugins section in ~/.zshrc
+git clone https://github.com/agkozak/zsh-z $ZSH_CUSTOM/plugins/zsh-z
+
