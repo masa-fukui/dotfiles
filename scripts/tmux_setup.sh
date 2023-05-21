4b87a69b3fd2b4b58e10edd13f86d9f4855646ec
Masa (Mark) Fukui <masahiro.m.fukui@gmail.com>
2023-05-21 15:59:38 +0900

    add tmux config files

---
+++ scripts/tmux_setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# install tpm
+if [ -d ~/.tmux/plugins/tpm ]; then
+    echo "tmux plugin manager already exists."
+else
+    echo "installing tmux plugin manager"
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+    # set default settings for tmux plugin manager
+    cat << EOF >> ~/.tmux.conf
+# List of plugins
+set -g @plugin 'tmux-plugins/tpm'
+set -g @plugin 'tmux-plugins/tmux-sensible'
+
+# Other examples:
+# set -g @plugin 'github_username/plugin_name'
+# set -g @plugin 'github_username/plugin_name#branch'
+# set -g @plugin 'git@github.com:user/plugin'
+# set -g @plugin 'git@bitbucket.com:user/plugin'
+
+# Initialize TMUX plugin manager (keep this line at the very bottom of tmux.conf)
+run '~/.tmux/plugins/tpm/tpm'
+EOF
+fi
