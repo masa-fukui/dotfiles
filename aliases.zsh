9a1e8f00d9676a38bfc844708417b689554f7f28
Masa (Mark) Fukui <masahiro.m.fukui@gmail.com>
2023-06-13 13:14:20 +0900

    feat: rename alias files

--- aliases.zsh
+++ aliases.zsh
@@ -5,3 +5,5 @@ alias elt="exa -lT"
 alias rustbook='open -a "Google Chrome" ~/workspace/book/book/index.html'
 alias bcommit='~/personal/gum_utils/better_commit.sh'
 alias bswitch='~/personal/gum_utils/better_switch.sh'
+
+alias c=clear
