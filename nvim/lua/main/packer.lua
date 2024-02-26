abb1428ac2c8035f2600bbf5abe5318b6452a0aa
Masamerc <masahiro.m.fukui@gmail.com>
2024-02-26 17:06:21 +0900

    feat: add nvimtree

--- nvim/lua/main/packer.lua
+++ nvim/lua/main/packer.lua
@@ -48,6 +48,20 @@ return require('packer').startup(function(use)
   use {
     "ThePrimeagen/vim-be-good"
   }
+  use {
+    "kyazdani42/nvim-tree.lua",
+    requires = "kyazdani42/nvim-web-devicons" ,
+    wants = "nvim-web-devicons",
+    config = function()
+      require("nvim-web-devicons").setup()
 
+      require("nvim-tree").setup {
+        hijack_cursor = true,
+        view = {
+          width = 40
+        }
+      }
+    end
+  }
 end)
 
