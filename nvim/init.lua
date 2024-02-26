b177d0241daddc98eda7f31a402dad1a148e2cdf
Masamerc <masahiro.m.fukui@gmail.com>
2024-02-26 22:44:21 +0900

    feat: add key for hover

--- nvim/init.lua
+++ nvim/init.lua
@@ -25,5 +25,14 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+  vim.lsp.handlers.hover,
+  {
+    border = "single", -- "shadow" , "none", "rounded"
+    -- border = border
+    -- width = 100,
+  }
+)
+vim.api.nvim_buf_set_keymap(0, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', {noremap = true, silent = true})
 
 LineNumberColors()
