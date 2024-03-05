85a980163bc5121df1019551656b8bd98826b893
Masa Fukui <masahiro.fukui@nowcast.co.jp>
2024-03-05 09:22:40 +0900

    feat: add catpuccin

--- nvim/lua/main/packer.lua
+++ nvim/lua/main/packer.lua
@@ -113,4 +113,5 @@ return require("packer").startup(function(use)
 	use({ "akinsho/bufferline.nvim", tag = "*", requires = "nvim-tree/nvim-web-devicons" })
     use({"ThePrimeagen/git-worktree.nvim"})
 
+    use { "catppuccin/nvim", as = "catppuccin" }
 end)
