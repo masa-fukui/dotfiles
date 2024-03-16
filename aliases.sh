7223732df9b125a98abda183bb6173945e346066
Masamerc <masahiro.m.fukui@gmail.com>
2024-03-16 19:43:05 +0900

    feat: rustbook alias

--- aliases.sh
+++ aliases.sh
@@ -37,7 +37,7 @@ alias ll="exa -l"
 alias et="exa -T"
 alias elt="exa -lT"
 
-alias rustbook='open -a "Google Chrome" ~/workspace/book/book/index.html'
+alias rustbook='rustup doc --book'
 alias gcommit='~/dotfiles/scripts/gum/better_commit.sh'
 alias gswitch='~/dotfiles/scripts/gum/better_switch.sh'
 
