098aea4fa0b9cc8a41b3a7f33a896ecf8cae70a7
Masamerc <masahiro.m.fukui@gmail.com>
2024-03-20 10:12:39 +0900

    feat: conceallevel 0 for md files

--- nvim/init.lua
+++ nvim/init.lua
@@ -59,7 +59,7 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 -- set conceallevel = 0 for md files
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = "md",
+    pattern = "markdown",
     callback = function()
         vim.opt_local.conceallevel = 0
     end,
