da57adcf34cb8e1d3d84239646c434609f008e24
Masa Fukui <masahiro.m.fukui@gmail.com>
2024-05-08 21:05:19 +0900

    update

--- nvim/lua/plugins/nvim-notify.lua
+++ nvim/lua/plugins/nvim-notify.lua
@@ -1,15 +1,6 @@
 require("notify").setup({
+    render = "minimal",
     stages = "fade_in_slide_out",
-    timeout = 3000,
-    background_colour = "#000000",
-    text_colour = "#d0d0d0",
-    icons = {
-        ERROR = "",
-        WARN = "",
-        INFO = "",
-        DEBUG = "",
-        TRACE = "✎",
-    },
 })
 -- nvim-notify
 vim.keymap.set("n", "<leader>sn", function()
