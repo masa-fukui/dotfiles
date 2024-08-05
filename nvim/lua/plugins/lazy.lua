5307eb46850b19ada82bdc614f87e6f1fb6a48cc
Masa Fukui <masahiro.fukui@nowcast.co.jp>
2024-08-06 00:13:24 +0900

    update

--- nvim/lua/plugins/lazy.lua
+++ nvim/lua/plugins/lazy.lua
@@ -274,6 +274,18 @@ local plugins = {
             })
         end
     },
+    {
+      "0xstepit/flow.nvim",
+      lazy = false,
+      priority = 1000,
+      config = function()
+        require("flow").setup({
+            transparent = true, -- Set transparent background.
+            fluo_color = "pink", --  Fluo color: pink, yellow, orange, or green.
+            mode = "medium", -- Intensity of the palette: normal, bright, desaturate, or dark. Notice that dark is ugly!
+        })
+        end,
+    },
 }
 
 local opts = {
