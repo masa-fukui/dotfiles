8b6fe2a767a35b0956bb58535a5eff463924d315
Masa Fukui <masahiro.m.fukui@gmail.com>
2024-08-18 07:18:49 +0000

    update lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -4,16 +4,28 @@ local options
 if workenv == "work" then
 	options = {
 		theme = "nordic",
+        icons_enabled = true,
+        disabled_filetypes = { 'neo-tree'},
 	}
 else
 	options = {
 		theme = "auto",
+        icons_enabled = true,
+        disabled_filetypes = { 'neo-tree'},
 	}
 end
 
 require("lualine").setup({
 	options = options,
 	sections = {
+        lualine_a = {
+          {
+            'mode',
+            fmt = function(str)
+              return ' ' .. str
+            end,
+          },
+        },
 		lualine_c = {
 			{
 				"filename",
