0b6896a95c8b2f60725fe5bcb998ae33604fd8ae
Masa Fukui <masahiro.fukui@nowcast.co.jp>
2024-08-19 09:18:31 +0900

    tmp

--- nvim/lua/plugins/lazy.lua
+++ nvim/lua/plugins/lazy.lua
@@ -265,15 +265,15 @@ local plugins = {
 			})
 		end,
 	},
-	{
-		"Exafunction/codeium.vim",
-		event = "BufEnter",
-		config = function()
-			vim.keymap.set("i", "<C-]>", function()
-				return vim.fn["codeium#CycleCompletions"](1)
-			end, { expr = true, silent = true })
-		end,
-	},
+	-- {
+	-- 	"Exafunction/codeium.vim",
+	-- 	event = "BufEnter",
+	-- 	config = function()
+	-- 		vim.keymap.set("i", "<C-]>", function()
+	-- 			return vim.fn["codeium#CycleCompletions"](1)
+	-- 		end, { expr = true, silent = true })
+	-- 	end,
+	-- },
 }
 
 local opts = {}
